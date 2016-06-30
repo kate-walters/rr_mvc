9d118dad5171e74d77cfa7aceb0207a2d38f6412
Kate Walters <kawalters01@gmail.com>
2016-06-30 13:15:34 -0500

    Post class added.

---
+++ app/models/post.rb
@@ -0,0 +1,11 @@
+class Post
+  attr_accessor :id, :title, :author, :body, :published
+
+  def intialize
+    @id = id
+    @title = title
+    @author = author
+    @body = body
+    @published = published
+  end
+end
