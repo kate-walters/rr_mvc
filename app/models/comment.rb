309e34339254c337029237cc1ed2d3d543e69e63
Kate Walters <kawalters01@gmail.com>
2016-06-30 14:06:17 -0500

    added unique id methods to comment and post classes.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,33 @@
+require 'faker'
 class Comment
-  attr_accessor :id, :message, :author, :post_id
+  @@all = []
+  attr_reader :id
+  attr_accessor :message, :author, :post_id
 
-  def initialize(id, message, author, post_id)
-    @id = id
+  def initialize(message, author, post_id)
+    @id = set_id
     @message = message
     @author = author
     @post_id = post_id
+  end
+
+  def to_json(json_arg = nil)
+    {
+      id: @id,
+      message: @message,
+      author: @author,
+      post_id: @post_id
+    }.to_json
+  end
+
+  def Comment.all
+    @@all
+  end
+
+  private
+  
+  def set_id
+    $user_id ||= 0
+    $user_id += 1
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,26 @@
 class Post
+  @@all = []
   attr_accessor :id, :title, :author, :body, :published
 
-  def intialize
+  def intialize (id, title, author, body, published)
     @id = id
     @title = title
     @author = author
     @body = body
     @published = published
+  end
+
+  def to_json(json_arg = nil)
+    {
+      id: @id,
+      title: @title,
+      author: @author,
+      body: @body,
+      published: @published
+    }.to_json
+  end
+
+  def Post.all
+    @@all
   end
 end
